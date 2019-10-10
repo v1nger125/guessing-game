c6a1501d4e1dba75d75f5e981d92457e375b52f1
Dmitriq Berlin <dmtrbrln@gmail.com>
2019-10-10 12:52:13 +0400

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.result;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.max + this.min) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
     }
 
     greater() {
-
+        this.min = this.result;
     }
 }
 
